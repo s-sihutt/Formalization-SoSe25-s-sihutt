0e6a4d94893505e663ecce42fc5be03ba89f2b56
Nima Rasekh <nima.rasekh@uni-greifswald.de>
2025-07-01 10:25:45 +0200

    new lean and lecture11

---
+++ FormalizationSoSe25/Lectures/Lecture11.lean
@@ -0,0 +1,24 @@
+/-
+Last time we did a summary of what we have done so far, and what remains to be done.
+-/
+
+section WhereToGoFromHere?
+/-
+If you want to pursue this further, here are some suggestions:
+- Check out the `Zulip` channel for `Lean4`: `https://leanprover.zulipchat.com/`
+- Check out some other languages that are similar to `Lean 4`:
+  * `Agda`
+  * `Rocq`
+- You could consider a Bachelor or Master thesis in this direction.
+
+In all of these cases, come talk to me!
+-/
+end WhereToGoFromHere?
+
+section WhatIsLeftToDo?
+/-
+1. Finish your project!
+2. Prepare your talk!
+3. You can also work on the exercises.
+-/
+end WhatIsLeftToDo?
